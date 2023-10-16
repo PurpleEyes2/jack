0343e55895b32fce1aafbb8281aeb6112237d02b
PurpleEyes2 <109381887+PurpleEyes2@users.noreply.github.com>
2023-10-16 16:42:28 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,29 @@
+import math
 
+print("Какую действие вы хотите сделать: сложение- a+b, вычитание- a-b, умножение - a*b, деление - a : b")
+answer = input("Введите действие")
+a = int(input("Введите число a"))
+b = int(input("Введите число b"))
+y = "y"
+while y == "y":
+    if answer == "сложение":
+        sum = a + b
+        print(sum)
+        print("Хотите попробовать другое действие?")
+        break
+    elif answer == "вычитание":
+        cub = a - b
+        print(cub)
+        print("Хотите поробовать другое действие?")
+        break
+    elif answer == "умножение":
+        milt = a * b
+        print(milt)
+        print("Хотите поробовать другое действие?")
+        break
+    elif answer == "умножение":
+        nmn = a / b
+        print(nmn)
+        print("Хотите поробовать другое действие?")
+        break 
+      
